b33bffc04ea6a9c93d330d5b60c191f3ee14e581
gxdsetmxnsters <59113894+gxdsetmxnsters@users.noreply.github.com>
2026-02-17 19:48:19 -0700

    Update bot.py
    Signed-off-by: gxdsetmxnsters <59113894+gxdsetmxnsters@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -1442,14 +1442,16 @@ class ModmailBot(commands.Bot):
         await self.config.update()
 
     async def on_message(self, message):
-        await self.wait_for_connected()
+    await self.wait_for_connected()
+    
     if not hasattr(self, '_plugin_handled_dm'):
         self._plugin_handled_dm = {}
     if self._plugin_handled_dm.get(message.author.id):
         del self._plugin_handled_dm[message.author.id]
         return
-        if message.type == discord.MessageType.pins_add and message.author == self.user:
-            await message.delete()
+    
+    if message.type == discord.MessageType.pins_add and message.author == self.user:
+        await message.delete()
 
         if (
             (f"<@{self.user.id}" in message.content or f"<@!{self.user.id}" in message.content)
